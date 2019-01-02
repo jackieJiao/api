cdb44050d140a717fb5714a347237614b97df5d9
jiaozhijun <jiaozhijun@hotmail.com>
2019-01-02 21:00:18 +0800

    test

---
+++ src/test/java/com/rongshu/api/test/EnumClass.java
@@ -0,0 +1,11 @@
+package com.rongshu.api.test;
+
+enum Shrubbery {G,C,H,Z
+}
+public class EnumClass
+{
+    public static void main(String[] args) {
+        String s="jiao";
+        System.out.println(s.getClass().getEnumConstants()[0]);
+    }
+}
